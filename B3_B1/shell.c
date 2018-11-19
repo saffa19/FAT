f4c0386f4660ccfe417e19ffc42a1aa4e098e963
Duncan <duncanvdw@gmail.com>
2018-11-19 23:50:28 +0000

    comment out code from prev CGS bands

--- B3_B1/shell.c
+++ B3_B1/shell.c
@@ -1,23 +1,18 @@
 /*
-Criteria for C3_C1:
+Criteria for B3_B1:
 
+Add a directory hierarchy to your virtualdisk that allows the creation of subdirectories. 
 Implement the following interface functions:
-- myfopen()
-- myfputc()
-- myfgetc()
-- myfclose()
+- mymkdir( char * path) that creates a new directory
+- char ** mylistdir (char * path) that lists the content of a directory
 
-It is assumed that there is only a root directory and that all files are created there. 
-Extend your test program shell.c with the following steps:
-- create a file “testfile.txt” in your virtual disk: call myfopen ( “testfile.txt”, “w” ) to open this file
-- write a text of size 4kb (4096 bytes) to this file, using the function myfputc():
-	- in shell.c, create a char array of 4 * BLOCKSIZE, fill it with text and then write it to the virtual file with myfputc()
-- close the file with myfclose()
-- write the complete virtual disk to a file “virtualdiskC3_C1”
-- test myfgetc():
-	- open the file again on your virtual hard disk
-	- read out its content with myfgetc() (you may read until the function returns EOF) and, at the same time, print it to the screen
-	- write the content to a real file on your real hard disk and call it “testfileC3_C1_copy.txt”
+Extend your test program shell.c with the following test steps:
+- create a directory “/myfirstdir/myseconddir/mythirddir” in the virtual disk
+- call mylistdir(“/myfirstdir/myseconddir”): print out the list of strings returned by this function
+- write out virtual disk to “virtualdiskB3_B1_a”
+- create a file “/myfirstdir/myseconddir/testfile.txt” in the virtual disk
+- call mylistdir(“/myfirstdir/myseconddir”): print out the list of strings returned by this function
+- write out virtual disk to “virtualdiskB3_B1_b”
 */
 
 #include <stdio.h>
@@ -27,12 +22,12 @@ Extend your test program shell.c with the following steps:
 #include "filesys.h"
 
 void cgsB3_B1(){
-	format("CS3026 Operating Systems Assessment 2018") ;
+/*	format("CS3026 Operating Systems Assessment 2018") ;
 	writedisk("virtualdiskD3_D1\0") ;
 	
 	// create a fake file "testfile.txt" in your virtual disk
 	MyFILE * fakeFile = myfopen("testfile.txt", "w") ;
-	writedisk("virtualdiskC3_C1\0") ;
+	//writedisk("virtualdiskC3_C1\0") ;
 	
 	// write body of text (4096 bytes) to the fake file
 	char * string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
@@ -68,9 +63,23 @@ void cgsB3_B1(){
 		}
 	}
 	printf("\n\n") ;
+*/
+	format("Block 0") ;
+	
+	// it should be easier to add the directories like this.
+	// can pull only last element of the path, check if it's a directory,
+	mymkdir("/myfirstdir") ;
+	mymkdir("/myfirstdir/myseconddir") ;
+	mymkdir("/myfirstdir/myseconddir/mythirddir") ;
+	
+	char** list ;
+	list = mylistdir("/myfirstdir/myseconddir") ;
 
-	format("CS3026 Operating Systems Assessment 2018") ;
-	writedisk("virtualdiskB3_B1\0") ;
+	writedisk("virtualdiskB3_B1_a\0") ;
+
+
+
+	writedisk("virtualdiskB3_B1_b\0") ;
 
 
 
