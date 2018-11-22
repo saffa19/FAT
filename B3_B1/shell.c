2a3183a22a4d16dcd4093d69695bd362e7b9abe6
Duncan <duncanvdw@gmail.com>
2018-11-22 00:15:59 +0000

    slimming down

--- B3_B1/shell.c
+++ B3_B1/shell.c
@@ -22,69 +22,24 @@ Extend your test program shell.c with the following test steps:
 #include "filesys.h"
 
 void cgsB3_B1(){
-/*	format("CS3026 Operating Systems Assessment 2018") ;
-	writedisk("virtualdiskD3_D1\0") ;
+	format("CS3026 Operating Systems Assessment 2018") ;
 	
-	// create a fake file "testfile.txt" in your virtual disk
+	char * path = "/myfirstdir/myseconddir/mythirddir" ;
+	mymkdir(path) ;
+
 	MyFILE * fakeFile = myfopen("testfile.txt", "w") ;
-	//writedisk("virtualdiskC3_C1\0") ;
-	
-	// write body of text (4096 bytes) to the fake file
 	char * string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-	int i, j;
+	int i, j ;
 	for (i = 0; i < 4*BLOCKSIZE; i++){
 		myfputc(string[j], fakeFile) ;
 		j++ ;
-		if (j == 26) j = 0;
+		if (j == 26) j = 0 ;
 	}
 	myfputc(EOF, fakeFile) ;
-	
-	// close the fake file
 	myfclose(fakeFile) ;
 
-	// write the complete virtual disk to a file virtualdiskC3_C1
-	writedisk("virtualdiskC3_C1\0") ;
-
-	// open the file on the virtual disk
-	myfopen("fakeFile", "r") ;
-
-	// open a file to be written to local hard disk
-	FILE * realFile;
-	char lastcharacter ;
-	realFile = fopen("testfileC3_C1_copy.txt", "w");
-
-	// print out fakeFile's content to the screen and save it to testfileC3_C1_copy.txt
-	printf("This is the content of testfileC3_C1_copy.txt:\n") ;
-	while (lastcharacter != EOF){
-		lastcharacter = myfgetc(fakeFile) ;
-		if (lastcharacter != EOF){
-			printf("%c", lastcharacter);
-			fprintf(realFile, "%c",lastcharacter);
-		}
-	}
-	printf("\n\n") ;
-*/
-	format("Block 0") ;
-	
-	// it should be easier to add the directories like this.
-	// can pull only last element of the path, check if it's a directory,
-	mymkdir("/myfirstdir") ;
-	mymkdir("/myfirstdir/myseconddir") ;
-	mymkdir("/myfirstdir/myseconddir/mythirddir") ;
-	
-	char** list ;
-	list = mylistdir("/myfirstdir/myseconddir") ;
-
 	writedisk("virtualdiskB3_B1_a\0") ;
-
-
-
 	writedisk("virtualdiskB3_B1_b\0") ;
-
-
-
-
-
 }
 
 int main(){
